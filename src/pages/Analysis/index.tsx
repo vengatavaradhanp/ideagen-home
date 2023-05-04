7c6e87323f6fd567040495a6559531b3f25f1179
vengatavaradhan <vengatavaradhan@twilightsoftwares.com>
2023-05-04 10:20:10 +0530

    filter code worked on analysis page

--- src/pages/Analysis/index.tsx
+++ src/pages/Analysis/index.tsx
@@ -15,17 +15,35 @@ import {
 import ReactEcharts from "echarts-for-react";
 import { useNavigate } from "react-router-dom";
 export default function Analysis(props: any) {
-    const [dataShow , setdataShow] = useState<any>({})
-    const navigate = useNavigate();
     const location = useLocation();
+
+    const [dataShow , setdataShow] = useState<any>({})
+
+    const [selectedValue, setSelectedValue] = useState<any>(location.state===null?
+        "all":location.state.name);
+    const navigate = useNavigate();
     // console.log(Object.keys(dataShow));
     // if(Object.keys(dataShow).length === 0){
+        
+
         useEffect(() => {
-            setdataShow(location.state);
-          }, []);
+          setdataShow(location.state || {});
+        }, [location.state]);
         // setdataShow(location.state)
 // }
     // console.log("loaction",typeof dataShow.selling)
+    const allData = [
+        { name: "Academy", selling: 32 },
+        { name: "Courson", selling: 50 },
+        { name: "Huddle", selling: 60 },
+        { name: "Pentana", selling: 80 },
+        { name: "OpsBase", selling: 65 },
+        { name: "PleaseReview", selling: 71 },
+        { name: "Q-Pulse*PM", selling: 20 },
+        { name: "Q-Pulse*", selling: 40 },
+      ];
+    console.log("data:",selectedValue)
+    const filteredData = selectedValue === "all"? allData : [dataShow];
     const option = {
         tooltip: {
           trigger: 'axis',
@@ -33,6 +51,9 @@ export default function Analysis(props: any) {
             type: 'shadow'
           }
         },
+        legend: {
+            
+          },
         grid: {
           left: '3%',
           right: '4%',
@@ -44,10 +65,12 @@ export default function Analysis(props: any) {
             
             type: 'category',
             // data: ['Academy', 'Courson', 'Huddle', 'Pentana', 'OpsBase', 'PleaseReview', 'Q-Pulse*','Q-Pulse*PM'],
-            data : [dataShow.name],
+            // data : [dataShow.name],
+            data: filteredData.map((data: any) => data.name),
             axisTick: {
               alignWithLabel: true
-            }
+            },
+           
           }
         ],
         yAxis: [
@@ -59,35 +82,39 @@ export default function Analysis(props: any) {
           {
             name: 'Direct',
             type: 'bar',
-            barWidth: '5%',
+            barWidth: '10%',
         
-            data : [parseInt(dataShow.selling)],
+            // data : [parseInt(dataShow.selling)],
+            data: filteredData.map((data: any) => parseInt(data.selling)),
             
             // data: [10, 52, 200, 334, 390, 330, 220,50]
-          }
+          },
           
         ]
       };
   return (
     
     <div>
-        <div style={{ padding: "15px 0px" }}>
+        <div style={{ padding: "15px 15px 5px 65px" }}>
           <Typography variant="h4">Analysis</Typography>
         </div>
-        <div>
-        <select style={{width:"200px",height:"25px"}} >
-        <option value="All">All</option>
-        <option value="Iphone">Academy</option>
-        <option value="Samsung">Courson</option>
-        <option value="Oneplus">Oneplus</option>
-        <option value="Oneplus">Huddle</option>
-        <option value="Oneplus">Pentana</option>
-        <option value="Oneplus">OpsBase</option>
-        <option value="Oneplus">PleaseReview</option>
-        <option value="Oneplus">Q-Pulse*</option>
-        <option value="Oneplus">Q-Pulse*PM</option>
-      </select>
-      </div>
+        <div style={{display:"flex",justifyContent:"end",padding:"0px 68px 0px 0px"}} >
+                  <TextField
+                  style={{width:"20%"}}
+                    id="outlined-basic"
+                    placeholder="All"
+                    value={selectedValue}
+                    variant="outlined"
+                    select
+                    fullWidth
+                    onChange={(e) => { setSelectedValue(e.target.value)}}
+          >
+            <MenuItem value="all">All</MenuItem>
+            {allData.map((data: any) => (
+            <MenuItem value={data.name}>{data.name}</MenuItem>
+            ))} 
+        </TextField>
+                </div>
         <div style={{height:"500px!important"}}>
         <ReactEcharts option={option} style={{height:"580px"}} />
         </div>
